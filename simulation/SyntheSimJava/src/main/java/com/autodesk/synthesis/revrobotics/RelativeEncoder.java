a36b1a5a302d74bbb5ed9e3468bbd403c1f55547
ktvuwhs <ktvu@hbuhsd.edu>
2025-01-05 01:56:15 -0800

    testing encoder

--- simulation/SyntheSimJava/src/main/java/com/autodesk/synthesis/revrobotics/RelativeEncoder.java
+++ simulation/SyntheSimJava/src/main/java/com/autodesk/synthesis/revrobotics/RelativeEncoder.java
@@ -5,7 +5,9 @@ import com.revrobotics.REVLibError;
 
 public class RelativeEncoder implements com.revrobotics.RelativeEncoder {
 
+    // TODO: Try changing m_original to a REV SparkMax and use REV SparkMax configAccess to re-add conversion ratios to get methods
     private com.revrobotics.RelativeEncoder m_original;
+    private double m_zero = 0.0;
     private CANEncoder m_encoder;
 
     public RelativeEncoder(com.revrobotics.RelativeEncoder original, CANEncoder encoder) {
@@ -15,7 +17,7 @@ public class RelativeEncoder implements com.revrobotics.RelativeEncoder {
 
     @Override
     public double getPosition() {
-        return m_encoder.getPosition();
+        return m_encoder.getPosition() - m_zero;
     }
 
     @Override
@@ -25,7 +27,7 @@ public class RelativeEncoder implements com.revrobotics.RelativeEncoder {
 
     @Override
     public REVLibError setPosition(double position) {
-        m_zero = m_encoder.getPosition();
+        m_zero = m_encoder.getPosition() - position;
         return REVLibError.kOk;
     }
     
